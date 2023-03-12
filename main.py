2e719a3bc00756dfdc391d288787a96abff9a649
Jakub Kubik <jakupkubik@gmail.com>
2023-03-12 22:30:31 +0100

    Setup base repository structure

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+def main():
+    pass
+    # TODO:
+    #  requirements.in, *.txt
+    #  nasetupovat structlog
+    #  rozvetvenie podla args
+    # yaml configy
+
+
+if __name__ == "__main__":
+    main()
