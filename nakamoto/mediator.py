a1cf7d4d98a167d30e416a914e6bba123047df28
Jakub Kubik <jakupkubik@gmail.com>
2023-03-15 21:09:52 +0100

    mediator: create method for simulation config validation

--- nakamoto/mediator.py
+++ nakamoto/mediator.py
@@ -11,11 +11,15 @@ from base.mediator_base import MediatorBase
 class Mediator(MediatorBase):
     """Mediator class for Nakamoto consensus for running whole simulation."""
 
+    def validate_blockchain_config(self):
+        self.log.info("validating simulation config")
+        print(type(self.config))
+        print(self.config)
+
     def run(self):
         """This method is entry point for running all checks for specific provider monitor."""
         self.log.info("Mediator in Nakamoto")
-        print(type(self.config))
-        print(self.config)
+        self.validate_blockchain_config()
 
     def parse_config(self, simulation_config):
         """Parsing dict from yaml config."""
