f49c5679500771c37d8ef465958aa4ab914b8759
Jakub Kubik <jakupkubik@gmail.com>
2023-03-26 18:40:44 +0200

    Subchain blockchain selfish mining on weak blocks

--- subchain/honest_miner.py
+++ subchain/honest_miner.py
@@ -4,14 +4,20 @@ honest miner for Subchain consensus protocol.
 Author: Jan Jakub Kubik (xkubik32)
 Date: 23.3.2023
 """
+import random
+
 from nakamoto.honest_miner import HonestMinerStrategy as NakamotoHonestMinerStrategy
 
 
 class HonestMinerStrategy(NakamotoHonestMinerStrategy):
     """Honest miner class implementation for Subchain consensus."""
 
-    # pylint: disable=unused-argument
-    @staticmethod
-    def select_subchain(ongoing_fork, competitors, public_subchain):
+    # pylint: disable=no-self-use
+    def select_subchain(self, ongoing_fork, competitors, public_subchain):
         """Select winning Subchain."""
-        print("")
+        if ongoing_fork:
+            winning_subchain = random.choice(competitors + [public_subchain])
+        else:
+            winning_subchain = public_subchain
+
+        return winning_subchain
