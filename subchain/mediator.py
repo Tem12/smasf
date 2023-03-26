2c48a294a3f1f2aa0dc8c06b99647d677380bc17
Jakub Kubik <jakupkubik@gmail.com>
2023-03-26 18:38:45 +0200

    Subchain blockchain simulation structure

--- subchain/mediator.py
+++ subchain/mediator.py
@@ -7,7 +7,11 @@ Date: 23.3.2023
 
 import json
 
+import numpy as np
+
 from base.blockchain import Blockchain
+from base.miner_base import MinerType
+from base.miner_base import SelfishMinerAction as SA
 from nakamoto.mediator import Mediator as NakamotoMediator
 from nakamoto.my_graphs import plot_block_counts
 from subchain.honest_miner import HonestMinerStrategy
@@ -55,7 +59,79 @@ class Mediator(NakamotoMediator):
 
     def run_simulation(self):
         """Main business logic for running selfish mining simulation."""
-        print("TODO")
+
+        # EXPONENTIAL GENERATION OF WEAK AND STRONG BLOCKS
+        # WHICH IS BETTER THAN UNIFORM DISTRIBUTION
+
+        # Average number of rounds between weak block generations
+        avg_rounds_between_weak_blocks = 1
+        # Average number of rounds between strong block generations
+        avg_rounds_between_strong_blocks = self.config.weak_to_strong_block_ratio
+        simulation_rounds = self.config.simulation_mining_rounds
+
+        # Generate random block generation rounds for weak and strong blocks
+        weak_block_rounds = np.random.exponential(
+            scale=avg_rounds_between_weak_blocks, size=simulation_rounds
+        ).cumsum()
+        strong_block_rounds = np.random.exponential(
+            scale=avg_rounds_between_strong_blocks, size=simulation_rounds
+        ).cumsum()
+        # Simulation loop
+        weak_block_idx = 0
+        strong_block_idx = 0
+
+        for blocks_mined in range(self.config.simulation_mining_rounds):
+            leader = self.choose_leader(self.miners, self.miners_info)
+
+            # Check if it's time to generate a weak block
+            if (
+                weak_block_idx < len(weak_block_rounds)
+                and blocks_mined >= weak_block_rounds[weak_block_idx]
+            ):
+                print(f"Weak block generated in round {blocks_mined}")
+                weak_block_idx += 1
+
+                self.one_round(leader, blocks_mined, is_weak_block=True)
+
+            # Check if it's time to generate a strong block
+            if (
+                strong_block_idx < len(strong_block_rounds)
+                and blocks_mined >= strong_block_rounds[strong_block_idx]
+            ):
+                print(f"Strong block generated in round {blocks_mined}")
+                print("-----------------" * 5)
+                strong_block_idx += 1
+
+                if leader.miner_type == MinerType.SELFISH:
+                    continue
+
+                competitors = self.action_store.get_objects(SA.MATCH)
+                competitors_blockchain = [
+                    competitor.blockchain for competitor in competitors
+                ]
+                selected_subchain = leader.select_subchain(
+                    self.ongoing_fork, competitors_blockchain, self.public_blockchain
+                )
+
+                self.public_blockchain_strong.chain.extend(selected_subchain)
+                self.public_blockchain_strong.add_block(
+                    data=f"Block {blocks_mined} data",
+                    miner=f"{'Honest' if leader.miner_type == MinerType.HONEST else 'Selfish'} "
+                    f"miner {leader.miner_id}",
+                    miner_id=leader.miner_id,
+                    is_weak=False,
+                )
+                self.action_store.clear()
+                self.ongoing_fork = False
+
+                self.public_blockchain.chain = []
+                self.public_blockchain.last_block_id = 0
+                self.public_blockchain.fork_block_id = None
+
+                for selfish_miner in self.selfish_miners:
+                    selfish_miner.blockchain.chain = []
+                    selfish_miner.blockchain.last_block_id = 0
+                    selfish_miner.blockchain.fork_block_id = None
 
     def run(self):
         self.log.info("Mediator in Subchain")
