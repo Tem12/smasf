bb1f8a54b6f3b491d8355fb43306532fd4857896
Jakub Kubik <jakupkubik@gmail.com>
2023-03-27 23:04:36 +0200

    xSubchain consensus with SM on strong blocks on my framework

---
+++ subchain/blockchain.py
@@ -0,0 +1,54 @@
+"""Module for class Nakamoto consensus blocks and blockchain.
+
+Author: Jan Jakub Kubik (xkubik32)
+Date: 23.3.2023
+"""
+from dataclasses import dataclass
+from typing import Optional
+
+from base.blockchain import Blockchain as NakamotoBlockchain
+
+
+@dataclass
+class Blockchain(NakamotoBlockchain):
+    """Blockchain class for Subchain consensus."""
+
+    last_strong_block_id: Optional[int] = 0
+
+    def size_from_index(self, index):
+        """Get length of strong blocks in the blockchain from specified index.
+
+        Args:
+            index (int): The index where to start counting of strong blocks
+        Returns:
+            int: Length of the blockchain.
+        """
+        size = 0
+        for block in self.chain[index:]:
+            # count on just strong blocks
+            if not block.is_weak:
+                size += 1
+
+        return size
+
+    def size(self):
+        """Get length of strong blocks in the blockchain.
+
+        Returns:
+            int: Length of the blockchain.
+        """
+        size = 0
+        for block in self.chain:
+            # count on just strong blocks
+            if not block.is_weak:
+                size += 1
+
+        return size
+
+    def length(self):
+        """Get length of private chain.
+
+        Returns:
+            int: Length of the private chain.
+        """
+        return self.size() + self.fork_block_id
--- subchain/honest_miner.py
+++ subchain/honest_miner.py
@@ -5,13 +5,16 @@ Author: Jan Jakub Kubik (xkubik32)
 Date: 23.3.2023
 """
 from nakamoto.honest_miner import HonestMinerStrategy as NakamotoHonestMinerStrategy
+from subchain.blockchain import Blockchain
 
 
 class HonestMinerStrategy(NakamotoHonestMinerStrategy):
     """Honest miner class implementation for Subchain consensus."""
 
-    # pylint: disable=unused-argument
-    @staticmethod
-    def select_subchain(ongoing_fork, competitors, public_subchain):
-        """Select winning Subchain."""
-        print("")
+    def __init__(self, mining_power: int):
+        super().__init__(mining_power)
+        self.blockchain_weak = Blockchain(owner="public blockchain weak")
+
+    def clear_private_weak_chain(self):
+        # public chain of weak blocks
+        self.blockchain_weak.chain = []
